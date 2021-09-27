e116e497577952e2a2869508c31b3d2a5b579ec7
OlgaSavitsk <olbonya.89@mail.ru>
2021-09-27 22:02:01 +0300

    feat:correspondence  pixelperfect

--- museum/index.js
+++ museum/index.js
@@ -4,3 +4,10 @@ progress.addEventListener('input', function() {
   const value = this.value;
   this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #fff ${value}%, white 100%)`
 })
+
+const progressVol = document.querySelector('.progress-volume');
+  
+progressVol.addEventListener('input', function() {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #fff ${value}%, white 100%)`
+})
