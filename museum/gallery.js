af7acbc772640df831b4013e0b776af4e1fc42e0
OlgaSavitsk <olbonya.89@mail.ru>
2021-10-13 00:01:59 +0300

    feat: add slide in om scroll

--- museum/gallery.js
+++ museum/gallery.js
@@ -25,3 +25,21 @@ window.onload = () => (
         pictureInnerContainer.innerHTML = srcImageNew.join('')
     })
 ) 
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    var reveals = document.querySelectorAll('.picture')
+    for(let i = 0; i < reveals.length; i++) {
+        let windowheight = window.innerHeight;
+        let revealtop = reveals[i].getBoundingClientRect().top;
+        let revealpoint = 150
+
+        if(revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('active-galery')
+        }
+        else {
+            reveals[i].classList.remove('active-galery')
+        }
+    }
+}
